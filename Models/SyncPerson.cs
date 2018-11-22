182355ab1a722143188a997f3eaf3c16e272432e
ll9 <llwierer@web.de>
2018-11-22 21:58:05 +0100

    is sync person entity

---
+++ Models/SyncPerson.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    class SyncPerson: Person
+    {
+        public bool IsSynced { get; set; }
+    }
+}
